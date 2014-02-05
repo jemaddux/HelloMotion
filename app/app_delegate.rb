cf2f941c1f021c46e9b76dad3c1363bcc838f766
John Maddux <jemaddux@gmail.com>
2014-02-04 22:10:01 -0500

    Alerts

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    alert = UIAlertView.new
+    alert.message = "Hello World!!!!!!!!!!!!"
+    alert.show
+
+    puts "Hello again!!!"
+
     true
   end
 end
