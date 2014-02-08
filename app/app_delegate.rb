78501ed519c2595229334edf105d55a6db3a3142
John Maddux <jemaddux@gmail.com>
2014-02-08 14:03:59 -0500

    Changed app delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,10 +1,12 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    alert = UIAlertView.new
-    alert.message = "Hello World!!!!!!!!!!!!"
-    alert.show
+    #UIScreen describes the display our app is running on.
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
 
-    puts "Hello again!!!"
+    @blue_view = UIView.alloc.initWithFrame(CGRectMake(10, 10, 100, 100))
+    @blue_view.backgroundColor = UIColor.blueColor
+    @window.assSubview(@blue_view)
 
     true
   end
