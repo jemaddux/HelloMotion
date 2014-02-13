7c1b2ea1233515cd0b11fbf4cafb37e32ccfb98c
John Maddux <jemaddux@gmail.com>
2014-02-12 23:19:34 -0500

    motiongit add -A!

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,21 +1,61 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    #UIScreen describes the display our app is running on.
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
     @window.makeKeyAndVisible
 
-    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
-    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
-    alphabet_controller = AlphabetController.alloc.initWithNibName(nil, bundle: nil)
+    # the points we are going to animate to
+    @points = [[0, 0], [0, 450], [50, 50], [220, 7], [234, 533]]
+    @current_index = 0
 
-    other_controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
-    other_controller.title = "Other"
-    other_controller.view.backgroundColor = UIColor.purpleColor
+    #usual method of adding view to our window
+    @view = UIView.alloc.initWithFrame [@points[@current_index], [100, 100]]
+    @view.backgroundColor = UIColor.blueColor
+    @window.addSubview(@view)
 
-    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
-    tab_controller.viewControllers = [alphabet_controller, nav_controller, other_controller]
-    @window.rootViewController = tab_controller
+    animate_to_next_point
+
+    # controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    # nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+    # alphabet_controller = AlphabetController.alloc.initWithNibName(nil, bundle: nil)
+
+    # other_controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
+    # other_controller.title = "Other"
+    # other_controller.view.backgroundColor = UIColor.purpleColor
+
+    # tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    # tab_controller.viewControllers = [alphabet_controller, nav_controller, other_controller]
+    # @window.rootViewController = tab_controller
 
     true
   end
+
+  def animate_to_next_point
+    @current_index += 1
+
+    # keep current index in range
+    @current_index = @current_index % @points.count
+
+    UIView.animateWithDuration(2,
+      animations: lambda {
+        @view.frame = [@points[@current_index], [100, 100]]
+      },
+      completion: lambda { |finished|
+        self.animate_to_next_point
+      }
+    )
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
